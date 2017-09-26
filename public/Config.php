3c482eaedd44e560685fe9d1097689961726ef03
che <rogachevich@g-soft.ru>
2017-09-26 16:57:53 +0300

    tmp

--- public/Config.php
+++ public/Config.php
@@ -8,8 +8,8 @@
  */
 class Config
 {
-    private $token = "7be80753195a2de690c4fcf35a8059c457a3a8540d0c61c742bd9aa14aa685ddd1e04401c1d5c37b6b318";
-    private $user = "133852802";
+    private $token = "";
+    private $user = "";
 
     public function getToken()
         {
